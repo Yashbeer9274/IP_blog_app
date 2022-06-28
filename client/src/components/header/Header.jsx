fe8157967bcf20769d119bc2444f940b3669e75c
riyaCrofarm <riya.kochhar@Crofarm.com>
2022-06-28 11:05:38 +0530

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">Blog</span>
+        {/* <span className="headerTitleSm">React & Node</span> */}
+        <span className="headerTitleLg">BLOG APP</span>
       </div>
       <img
         className="headerImg"
--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./topbar.css";
-
+// import { IoSettings } from "react-icons/io5";
+import { FaRegUserCircle } from "react-icons/fa";
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "http://localhost:3000/images/";
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
@@ -13,10 +14,14 @@ export default function TopBar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
+        {/* <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-twitter-square"></i>
         <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i> */}
+        <div className="topSearchIcon_div">
+          <i className="topSearchIcon fas fa-search"></i>
+          <input placeholder="Search" className="topSearchIcon_bar"></input>
+        </div>
       </div>
       <div className="topCenter">
         <ul className="topList">
@@ -48,7 +53,13 @@ export default function TopBar() {
       <div className="topRight">
         {user ? (
           <Link to="/settings">
-            <img className="topImg" src={PF+user.profilePic} alt="" />
+            {/* <IoSettings className="topImg" /> */}
+            {/* <FaRegUserCircle className="topImg" /> */}
+            {/* <img className="topImg" src={PF + user.profilePic} alt="" /> */}
+            <img
+              className="topImg"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiAY_GUgx2TF2_XUlRuGGefgo39motb-Zefh5CLRWF_3a-_BmI5PWulBOKXcFWfosKjEA&usqp=CAU"
+            />
           </Link>
         ) : (
           <ul className="topList">
@@ -64,7 +75,7 @@ export default function TopBar() {
             </li>
           </ul>
         )}
-        <i className="topSearchIcon fas fa-search"></i>
+        {/* <i className="topSearchIcon fas fa-search"></i> */}
       </div>
     </div>
   );
